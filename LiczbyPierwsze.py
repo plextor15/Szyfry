02cc05c794794b9a44046092b65dc5c35104d480
Adam Porembinski <75131243+plextor15@users.noreply.github.com>
2022-04-01 09:50:56 +0200

    working (debug)

--- LiczbyPierwsze.py
+++ LiczbyPierwsze.py
@@ -8,29 +8,39 @@
 def BruteDzielenie( n ):
     #pass
     #print("DEBUG ONLY Brute")
-    for i in range(2, n +1, 1):
-        print("debug only brute for")
-        if n % i == 0:
-            if i == n:
-                return True
-            else:
-                return False
+
+    #for i in range(2, n +1, 1):
+    i = 2
+    while i*i <= n:
+        #print("debug only brute for")
+        if n % i == 0 and i != n:
+            return False
+        i += 1
+
+    return True
+
 
 #Liczby Mersenne’a
 def Mersenn( p ):
     #pass
-    wynik = 2
-    if BruteDzielenie( p ):
-        for i in range( 0, p, 1 ):
-            print("debug only Mersenn for")
-            wynik *= 2
-    wynik = wynik - 1
+    #wynik = 1
+    #if BruteDzielenie( p ):
+    #    for i in range( 0, p, 1 ):
+    #        print("debug only Mersenn for")
+    #        wynik *= 2
+    #wynik = wynik - 1
 
-    wyjscie = [False, wynik]
-    if BruteDzielenie( wynik ):
-        wyjscie[0] = True
-    else:
-        wyjscie[0] = False
+    #wyjscie = [False, wynik]
+    #if BruteDzielenie( wynik ):
+    #    wyjscie[0] = True
+    #else:
+    #    wyjscie[0] = False
+
+    wyjscie = [True, 1]
+
+    for i in range( 1, p+1, 1 ):
+        wyjscie[1] *= 2
+    wyjscie[1] -= 1
 
     return wyjscie
 
@@ -39,12 +49,16 @@ def LukaszLehmer( P, M ):
     #pass
     S4 = 4
     Si = S4
+    i = 1
 
-    for i in range(P-2, 0 -1, 1):
-        print("debug only Lehmer for")
+    #for i in range(P-2, 0 -1, 1):
+    #    print("debug only Lehmer for")
+    #    Si = ((Si * Si) - 2) % M
+    while i <= P-2:
         Si = ((Si * Si) - 2) % M
+        i += 1
 
-    if Si == 0:
+    if Si % M == 0:
         return True
     else:
         return False
@@ -56,29 +70,33 @@ BruteTest = False
 linijka = [0, "NIE", "NIE"]
 
 print("\nZnajdywanie")
-while GlownyIter <= 7:
+while GlownyIter <= 18:
     #prosty odsiew polowy
     #if GlownyIter % 2 == 0 and GlownyIter != 2:
         #print("DEBUG ONLY - parzysta > 2")
         #continue
 
     BruteTest = BruteDzielenie( GlownyIter )
+    print( GlownyIter )
     if BruteTest:
 
-        print("DEBUG ONLY  ")
+        
         # I kolumna - p od Mersenna
         linijka[0] = GlownyIter
+        #print("DEBUG ONLY  ")
 
         # II kolumna - brute dzielenie czy wynik z Mersenne'a jest liczba pierwsza
         wyjscie_M = Mersenn( GlownyIter )
-        print("DEBUG ONLY  ")
+        print("DEBUG ONLY  M")
         if wyjscie_M[0]:
             linijka[1] = "TAK"
         else:
             linijka[1] = "NIE"
 
+
         # III kolumna - Lukasze Lehmerem czy wynik z Mersenne'a jest liczba pierwsza
         IIIkolumna = LukaszLehmer( GlownyIter, wyjscie_M[1] )
+        print("DEBUG ONLY  L")
         if IIIkolumna:
             linijka[2] = "TAK"
         else:
@@ -86,5 +104,9 @@ while GlownyIter <= 7:
 
         
         #na koniec petli
-        print("p = ", linijka[0], "   ", linijka[1],"   ", linijka[2], "\n")
-        GlownyIter += 1
+        print("p = ", linijka[0], "   ", linijka[1],"   ", linijka[2])
+        
+    GlownyIter += 1
+
+    #zeby nie przelatywalo za szybko
+    input("")
