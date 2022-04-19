9a44ff1e63c5c1699d5f05a3e168b1ae6b1cd325
Adam Porembinski <75131243+plextor15@users.noreply.github.com>
2022-04-19 12:25:13 +0200

    Add files via upload

--- func_powermod.py
+++ func_powermod.py
@@ -1,52 +1,53 @@
+from abc import ABC
 import math
+import random
+from typing import Callable
 
 # a^b mod c
 def PowerMod(a, b, c):
-    pass
-
-    if c==1:
-        return 0
+    if c==1: return 0
 
     #zamiana z BIN na DEC
     bin_b = bin(b)
     bin_b = bin_b[2:]
 
     #### DEBUG ONLY ####
-    print("DEC: ", b, "    BIN: ", bin_b, "\n\n")
-    
-    #liczby_do_zpot = []
-    #wielkosc = len(bin_b)
-    #pot = wielkosc-1
-    #for i in range(0, wielkosc, 1):
-    #    if bin_b[i] == '1':
-    #        liczby_do_zpot.append(pow(2,pot))
-    #    pot -= 1 
-    #maxwykladnik = len(bin_b)
+    #print("DEC: ", b, "    BIN: ", bin_b, "\n\n")
 
     poprzedni = 1
     wielkosc = len(bin_b)
     for i in range(0, wielkosc, 1):
         #postacbin[i]
         if bin_b[i]=="1":
-            print( "     ", poprzedni, "^2 * ", a, "^1 = ", poprzedni**2, " * ", a**1, " = ", (poprzedni**2) * (a**1))
+            #print( "     ", poprzedni, "^2 * ", a, "^1 = ", poprzedni**2, " * ", a**1, " = ", (poprzedni**2) * (a**1))
             poprzedni = (poprzedni**2) * (a**1)
         else:
-            print( "     ", poprzedni, "^2 * ", a, "^0 = ", poprzedni**2, " * ", a**0, " = ", (poprzedni**2) * (a**0))
+            #print( "     ", poprzedni, "^2 * ", a, "^0 = ", poprzedni**2, " * ", a**0, " = ", (poprzedni**2) * (a**0))
             poprzedni = (poprzedni**2) * (a**0)
         poprzedni = poprzedni % c
-        print( " ", i, "  ", bin_b[i], "  ", poprzedni )
+        #print( " ", i, "  ", bin_b[i], "  ", poprzedni )
 
     return poprzedni
 
 
 
-
-
-
 #main()
-lol = PowerMod(3,17,25)
-#for i in range(len(lol)): 
-#    print(lol[i])
+A = 0
+B = 0
+C = 0
+x = 0
+y = 0
+z = True
 
-print(lol)
-print(3**17%25)
+for i in range(0,100,1):
+    A = random.randint(100, 1000)
+    B = random.randint(100, 1000)
+    C = random.randint(100, 1000)
+    
+    x = PowerMod(A,B,C)
+    y = A**B%C
+
+    if x==y: z = True
+    else: z = False
+
+    print( i, "  -  ", x, "    ", y, "    ", z )
